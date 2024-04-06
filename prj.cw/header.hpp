4f03efc2f24dc353f323826373aafbe8de82be0a
ushakova23 <113995972+ushakova23@users.noreply.github.com>
2024-04-06 19:35:43 +0300

    Update header.hpp

--- prj.cw/header.hpp
+++ prj.cw/header.hpp
@@ -1,68 +1,24 @@
-#ifndef header_h
-#define header_h
-
+#ifndef VECTOR_MEDIAN_FILTER_HPP
+#ifndef IMAGE_PROCESSOR_HPP
+#define VECTOR_MEDIAN_FILTER_HPP
+#define IMAGE_PROCESSOR_HPP
 
 #include <opencv2/opencv.hpp>
+#include <vector>
+#include <algorithm>
 
-void centralProjection(cv::Mat& inputImage) {
-    // Проверка на правильность размеров входного изображения
-    if (inputImage.rows != inputImage.cols || inputImage.channels() != 3) {
-        std::cout << "Некорректные размеры входного изображения. Ожидается квадратное цветное изображение." << std::endl;
-        return;
-    }
+using namespace cv;
+using namespace std;
 
-    // Создание выходного изображения
-    cv::Mat outputImage(inputImage.rows, inputImage.cols, CV_8UC3, cv::Scalar(0, 0, 0));
+class VectorMedianFilter {
+public:
+    Vec3b calculateMedian(const vector<Vec3b>& pixels);
+};
 
-    // Расчет главной диагонали
-    int diagonalLength = inputImage.rows * std::sqrt(3);
+class ImageProcessor {
+public:
+    Mat applyVectorMedianFilter(const Mat& src, int kSize);
+};
 
-    // Проход по каждой точке входного изображения
-    for (int x = 0; x < inputImage.cols; x++) {
-        for (int y = 0; y < inputImage.rows; y++) {
-            // Вычисление координаты точки в 3D пространстве
-            double x3D = x - inputImage.cols / 2;
-            double y3D = y - inputImage.rows / 2;
-            double z3D = diagonalLength / 2;
-
-            // Проекция точки на плоскость, перпендикулярную главной диагонали
-            double scaleFactor = z3D / (z3D - y3D);
-            double projectedX = x3D * scaleFactor;
-            double projectedY = x3D * scaleFactor;
-
-            // Преобразование координат в индексы изображения
-            int outputX = static_cast<int>(projectedX + outputImage.cols / 2);
-            int outputY = static_cast<int>(projectedY + outputImage.rows / 2);
-
-            // Проверка границ выходного изображения
-            if (outputX >= 0 && outputX < outputImage.cols && outputY >= 0 && outputY < outputImage.rows) {
-                // Установка значения пикселя в выходном изображении
-                outputImage.at<cv::Vec3b>(outputY, outputX) = inputImage.at<cv::Vec3b>(y, x);
-            }
-        }
-    }
-
-    // Отображение выходного изображения
-    cv::imshow("Центральная проекция", outputImage);
-    cv::waitKey(0);
-
-    // Сохранение выходного изображения
-    cv::imwrite("output_image.jpg", outputImage);
-}
-
-/*int main() {
-    // Загрузка изображения
-    cv::Mat inputImage = cv::imread("input_image.jpg", cv::IMREAD_COLOR);
-
-    // Проверка на успешную загрузку изображения
-    if (inputImage.empty()) {
-        std::cout << "Не удалось загрузить изображение." << std::endl;
-        return -1;
-    }
-
-    // Вызов функции центральной проекции
-    centralProjection(inputImage);
-
-    return 0;
-}*/
-#endif header_h
+#endif // VECTOR_MEDIAN_FILTER_HPP
+#endif // IMAGE_PROCESSOR_HPP
