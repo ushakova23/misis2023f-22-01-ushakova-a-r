3f13440cd640489ca9182ed28302c580b5130201
ushakova23 <113995972+ushakova23@users.noreply.github.com>
2024-04-07 13:38:23 +0300

    Update median_filter.hpp

--- prj.cw/source/median_filter.hpp
+++ prj.cw/source/median_filter.hpp
@@ -1,1 +1,24 @@
+#ifndef VECTOR_MEDIAN_FILTER_HPP
+#ifndef IMAGE_PROCESSOR_HPP
+#define VECTOR_MEDIAN_FILTER_HPP
+#define IMAGE_PROCESSOR_HPP
 
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <algorithm>
+
+using namespace cv;
+using namespace std;
+
+class VectorMedianFilter {
+public:
+    Vec3b calculateMedian(const vector<Vec3b>& pixels);
+};
+
+class ImageProcessor {
+public:
+    Mat applyVectorMedianFilter(const Mat& src, int kSize);
+};
+
+#endif // VECTOR_MEDIAN_FILTER_HPP
+#endif // IMAGE_PROCESSOR_HPP
